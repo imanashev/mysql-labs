a1f2faa8299b6af4054e921e9d1844f385002ac7
Ildar Manashev <imanashev@gmail.com>
2019-05-29 16:42:11 +0700

    [+] script for lab1

---
+++ 1-two-instance/start-mysql.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DATA_DIR=${1}
+PORT=${2}
+SOCKET=${3}
+MYSQL_HOME_DIR=../mysql/mysql*/
+
+echo "Creating data dir"
+mkdir -p ${MYSQL_HOME_DIR}/${DATA_DIR}
+
+echo "Creating socket"
+touch ${MYSQL_HOME_DIR}/${SOCKET}
+
+echo "Initialization mysql with data dir: ${DATA_DIR}"
+${MYSQL_HOME_DIR}/bin/mysqld --initialize-insecure --user=root --datadir=${MYSQL_HOME_DIR}/${DATA_DIR} --basedir=.
+
+echo "Starting mysql at port ${PORT} and socket ${SOCKET}"
+${MYSQL_HOME_DIR}/bin/mysqld --datadir=${MYSQL_HOME_DIR}/${DATA_DIR} --basedir=. --port=${PORT} --socket=${MYSQL_HOME_DIR}/${SOCKET}
---
+++ 1-two-instance/two-instance.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Starting 1st instance"
+./start-mysql.sh mydata1 3306 sock.1
+
+echo ""
+echo "Starting 2nd instance"
+./start-mysql.sh mydata2 4000 sock.2
+
+
+echo ""
+echo "Check 1st instance"
+./check-mysql.sh 3306
+
+echo ""
+echo "Check 2nd instance"
+./check-mysql.sh 4000
+
+
+echo ""
+echo "Stopping 1st instance"
+./stop-mysql.sh 3306
+
+echo ""
+echo "Stopping 2nd instance"
+./stop-mysql.sh 4000
